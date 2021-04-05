d8c83c8a9c87c79a580d1ed0aed04e331fd15598
Vladimir Volkodav <42608405+Rise777@users.noreply.github.com>
2021-04-05 23:29:46 +0300

    Add files via upload

---
+++ Понимание SQL - Грубер/requests.sql
@@ -0,0 +1,36 @@
+-- SELECT snum, comm
+-- FROM Salepeople;
+-- SELECT odate, snum, onum, amt
+-- FROM Orders;
+-- SELECT DISTINCT snum
+-- FROM Orders;
+-- SELECT sname, city
+-- FROM Salepeople
+-- WHERE city = "London"
+-- SELECT *
+-- FROM Customers
+-- WHERE rating = 100;
+
+-- Работа с SQL
+
+-- 1 --
+-- SELECT onum, amt, odate
+-- FROM Orders;
+
+-- 2 --
+-- SELECT *
+-- FROM Customers
+-- WHERE snum = 1001;
+
+-- 3 --
+-- SELECT city, sname, snum, comm
+-- FROM Salepeople;
+
+-- 4 --
+-- SELECT rating, cname
+-- FROM Customers
+-- WHERE city = "San Jose";
+
+-- 5 --
+-- SELECT DISTINCT snum
+-- FROM Orders;
